f8f599a9de1fe1029f6f3302bc90752d5d9a4902
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-03-19 17:54:44 +0800

    add place and place service

---
+++ Guider.API.MVP/Models/Place.cs
@@ -0,0 +1,20 @@
+﻿namespace Guider.API.MVP.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+    public class Place
+    {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [BsonElement("category")]
+    public string Category { get; set; } = string.Empty;
+
+    [BsonElement("description")]
+    public string Description { get; set; } = string.Empty;
+}
+
---
+++ Guider.API.MVP/Services/PlaceService.cs
@@ -0,0 +1,32 @@
+﻿namespace Guider.API.MVP.Services
+{
+    using Guider.API.MVP.Models;
+    using MongoDB.Driver;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    public class PlaceService
+    {
+        private readonly IMongoCollection<Place> _placeCollection;
+
+        public PlaceService(IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase("guider"); // Замените на реальное имя базы
+            _placeCollection = database.GetCollection<Place>("places_with_location");
+        }
+
+        public async Task<List<Place>> GetAllAsync() =>
+            await _placeCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Place?> GetByIdAsync(string id) =>
+            await _placeCollection.Find(b => b.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Place place) =>
+            await _placeCollection.InsertOneAsync(place);
+
+        public async Task UpdateAsync(string id, Place updatedBusiness) =>
+            await _placeCollection.ReplaceOneAsync(b => b.Id == id, updatedBusiness);
+
+        public async Task DeleteAsync(string id) =>
+            await _placeCollection.DeleteOneAsync(b => b.Id == id);
+    }
+}
