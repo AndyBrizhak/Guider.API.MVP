dc1f9a2b2b2da08334ba469667e53d634ff7ba25
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-03-22 20:21:52 +0800

    fix place service

--- Guider.API.MVP/Services/PlaceService.cs
+++ Guider.API.MVP/Services/PlaceService.cs
@@ -1,32 +1,42 @@
 ﻿namespace Guider.API.MVP.Services
 {
     using Guider.API.MVP.Models;
+    using MongoDB.Bson;
     using MongoDB.Driver;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     public class PlaceService
     {
-        private readonly IMongoCollection<Place> _placeCollection;
+        private readonly IMongoCollection<BsonDocument> _placeCollection;
 
         public PlaceService(IMongoClient mongoClient)
         {
-            var database = mongoClient.GetDatabase("guider"); // Замените на реальное имя базы
-            _placeCollection = database.GetCollection<Place>("places_with_location");
+            var database = mongoClient.GetDatabase("guider"); 
+            _placeCollection = database.GetCollection<BsonDocument>("places_clear");
         }
 
-        public async Task<List<Place>> GetAllAsync() =>
+        public async Task<List<BsonDocument>> GetAllAsync() =>
             await _placeCollection.Find(_ => true).ToListAsync();
 
-        public async Task<Place?> GetByIdAsync(string id) =>
-            await _placeCollection.Find(b => b.Id == id).FirstOrDefaultAsync();
+        public async Task<BsonDocument?> GetByIdAsync(string id)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(id));
+            return await _placeCollection.Find(filter).FirstOrDefaultAsync();
+        }
 
-        public async Task CreateAsync(Place place) =>
+        public async Task CreateAsync(BsonDocument place) =>
             await _placeCollection.InsertOneAsync(place);
 
-        public async Task UpdateAsync(string id, Place updatedPlace) =>
-            await _placeCollection.ReplaceOneAsync(b => b.Id == id, updatedPlace);
+        public async Task UpdateAsync(string id, BsonDocument updatedPlace)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(id));
+            await _placeCollection.ReplaceOneAsync(filter, updatedPlace);
+        }
 
-        public async Task DeleteAsync(string id) =>
-            await _placeCollection.DeleteOneAsync(b => b.Id == id);
+        public async Task DeleteAsync(string id)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(id));
+            await _placeCollection.DeleteOneAsync(filter);
+        }
     }
 }
