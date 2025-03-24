e6fb70bfafea2489d8598b86e59d20a1d6a85cb3
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-03-24 15:19:25 +0800

    add geo query

--- Guider.API.MVP/Services/PlaceService.cs
+++ Guider.API.MVP/Services/PlaceService.cs
@@ -1,6 +1,7 @@
 ﻿namespace Guider.API.MVP.Services
 {
     using Guider.API.MVP.Models;
+    using Microsoft.Extensions.Options;
     using MongoDB.Bson;
     using MongoDB.Driver;
     using System.Collections.Generic;
@@ -9,34 +10,65 @@
     {
         private readonly IMongoCollection<BsonDocument> _placeCollection;
 
-        public PlaceService(IMongoClient mongoClient)
+        public PlaceService(IOptions<MongoDbSettings> mongoSettings)
         {
-            var database = mongoClient.GetDatabase("guider"); 
-            _placeCollection = database.GetCollection<BsonDocument>("places_clear");
+            var client = new MongoClient(mongoSettings.Value.ConnectionString);
+            var database = client.GetDatabase(mongoSettings.Value.DatabaseName);
+            _placeCollection = database.GetCollection<BsonDocument>(mongoSettings.Value.CollectionName);
         }
 
         public async Task<List<BsonDocument>> GetAllAsync() =>
             await _placeCollection.Find(_ => true).ToListAsync();
 
-        public async Task<BsonDocument?> GetByIdAsync(string id)
-        {
-            var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(id));
-            return await _placeCollection.Find(filter).FirstOrDefaultAsync();
-        }
+        public async Task<BsonDocument?> GetByIdAsync(string id) =>
+        await _placeCollection.Find(b => b["_id"] == ObjectId.Parse(id)).FirstOrDefaultAsync();
 
         public async Task CreateAsync(BsonDocument place) =>
             await _placeCollection.InsertOneAsync(place);
 
-        public async Task UpdateAsync(string id, BsonDocument updatedPlace)
-        {
-            var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(id));
-            await _placeCollection.ReplaceOneAsync(filter, updatedPlace);
-        }
+        //public async Task UpdateAsync(string id, BsonDocument updatedPlace)
+        //{
+        //    var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+        //    return await _placeCollection.ReplaceOneAsync(filter, updatedPlace);
+        //}
 
-        public async Task DeleteAsync(string id)
+        //public async Task DeleteAsync(string id)
+        //{
+        //    var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+        //    return await _placeCollection.DeleteOneAsync(filter);
+        //}
+
+        public async Task<string> GetPlacesNearbyAsync(decimal lat, decimal lng, int maxDistanceMeters)
         {
-            var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(id));
-            await _placeCollection.DeleteOneAsync(filter);
+            var pipeline = new[]
+            {
+            new BsonDocument("$geoNear", new BsonDocument
+            {
+                { "near", new BsonDocument
+                    {
+                        { "type", "Point" },
+                        { "coordinates", new BsonArray { lng, lat } }
+                    }
+                },
+                { "distanceField", "distance" },
+                { "maxDistance", maxDistanceMeters },
+                { "spherical", true }
+            }),
+            new BsonDocument("$project", new BsonDocument
+            {
+                { "_id", 1 },
+                { "distance", 1 },
+                { "name", 1 },
+                { "img_link", new BsonDocument
+                    {
+                        { "$arrayElemAt", new BsonArray { "$img_link", 0 } } // Первая ссылка на изображение
+                    }
+                }
+            })
+        };
+
+            var result = await _placeCollection.Aggregate<BsonDocument>(pipeline).ToListAsync();
+            return result.ToJson();
         }
     }
 }
