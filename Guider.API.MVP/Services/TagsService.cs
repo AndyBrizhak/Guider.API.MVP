fb1e4e7b930155eb4ee1e17dc981f8b595ecf0c1
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-04-22 09:17:21 +0800

    add tags service and get list tags service

---
+++ Guider.API.MVP/Services/TagsService.cs
@@ -0,0 +1,58 @@
+﻿using Guider.API.MVP.Data;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace Guider.API.MVP.Services
+{
+    public class TagsService
+    {
+        private readonly IMongoCollection<BsonDocument> _tagsCollection;
+        public TagsService(IOptions<MongoDbSettings> mongoSettings)
+        {
+            var client = new MongoClient(mongoSettings.Value.ConnectionString);
+            var database = client.GetDatabase(mongoSettings.Value.DatabaseName);
+            _tagsCollection = database.GetCollection<BsonDocument>(
+                mongoSettings.Value.Collections["Tags"]);
+        }
+
+        public async Task<JsonDocument> GetTagsByTypeAsync(string typeName)
+        {
+            try
+            {
+                var filter = Builders<BsonDocument>.Filter.Eq("name", typeName);
+                var projection = Builders<BsonDocument>.Projection.Include("tags").Exclude("_id");
+                var result = await _tagsCollection.Find(filter).Project(projection).FirstOrDefaultAsync();
+
+                if (result == null || !result.Contains("tags"))
+                {
+                    var errorResponse = new
+                    {
+                        IsSuccess = false,
+                        Message = "Type tags not found or no tags available."
+                    };
+                    return JsonDocument.Parse(JsonSerializer.Serialize(errorResponse));
+                }
+
+                var tags = result["tags"].AsBsonArray.Select(city => city.AsBsonDocument).ToList();
+                var successResponse = new
+                {
+                    IsSuccess = true,
+                    Tags = tags
+                }.ToJson();
+                return JsonDocument.Parse(successResponse);
+                
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = new
+                {
+                    IsSuccess = false,
+                    Message = $"An error occurred: {ex.Message}"
+                };
+                return JsonDocument.Parse(JsonSerializer.Serialize(errorResponse));
+            }
+        }
+    }
+}
