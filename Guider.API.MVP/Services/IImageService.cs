33f1a6d85e413a8d8d8374232154808c896a6378
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-04-25 16:05:15 +0800

    fix interface image srvice

--- Guider.API.MVP/Services/IImageService.cs
+++ Guider.API.MVP/Services/IImageService.cs
@@ -8,7 +8,7 @@ namespace Guider.API.MVP.Services
         JsonDocument GetImage(string fullPath);
         JsonDocument DeleteImage(string fullPath);
 
-        // Новый метод для обновления изображения
-        Task<string> UpdateImageAsync(string imagePath, IFormFile imageFile, bool createIfNotExists = true);
+        
+        //Task<string> UpdateImageAsync(string imagePath, IFormFile imageFile, bool createIfNotExists = true);
     }
 }
