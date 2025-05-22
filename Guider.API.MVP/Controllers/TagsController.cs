d68977bd0b0e10ab8e8d2bc171ece8f6c27f48c1
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-05-22 11:39:38 +0800

    clean code tags management

--- Guider.API.MVP/Controllers/TagsController.cs
+++ Guider.API.MVP/Controllers/TagsController.cs
@@ -1,27 +1,4 @@
-﻿//using Guider.API.MVP.Services;
-//using Guider.API.MVP.Utility;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Net;
-//using System.Text.Json;
-
-//namespace Guider.API.MVP.Controllers
-//{
-//    [Route("")]
-//    [ApiController]
-//    public class TagsController : ControllerBase
-//    {
-//        private readonly TagsService _tagsService;
-
-//        public TagsController(TagsService tagsService)
-//        {
-//            _tagsService = tagsService;
-//        }
-
-
-//    }
-//}
+﻿
 
 using Guider.API.MVP.Models;
 using Guider.API.MVP.Services;
--- Guider.API.MVP/Services/TagsService.cs
+++ Guider.API.MVP/Services/TagsService.cs
@@ -1,26 +1,4 @@
-﻿//using Guider.API.MVP.Data;
-//using Microsoft.Extensions.Options;
-//using MongoDB.Bson;
-//using MongoDB.Driver;
-//using System.Text.Json;
-
-//namespace Guider.API.MVP.Services
-//{
-//    public class TagsService
-//    {
-//        private readonly IMongoCollection<BsonDocument> _tagsCollection;
-//        public TagsService(IOptions<MongoDbSettings> mongoSettings)
-//        {
-//            var client = new MongoClient(mongoSettings.Value.ConnectionString);
-//            var database = client.GetDatabase(mongoSettings.Value.DatabaseName);
-//            _tagsCollection = database.GetCollection<BsonDocument>(
-//                mongoSettings.Value.Collections["Tags"]);
-//        }
-
-
-//    }
-//}
-
+﻿
 using Guider.API.MVP.Data;
 using Microsoft.Extensions.Options;
 using MongoDB.Bson;
